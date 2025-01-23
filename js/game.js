43fa01a3428f7d89428e388dbfd77a8c2edb74d4
M0dius <mohwalalawi@gmail.com>
2025-01-23 06:15:37 +0300

    enemies now shoot lasers at random intervals, without collisions

--- js/game.js
+++ js/game.js
@@ -4,16 +4,19 @@ const $gameContainer = document.querySelector('.game')
 const GAME_WIDTH = 800
 const GAME_HEIGHT = 600
 
+/*PLAYER PROPERTIES*/
 const PLAYER_WIDTH = 60
-//all speeds are in pixles/s
+//all speeds are in milliseconds
 const PLAYER_MAX_SPEED = 300
 const LASER_MAX_SPEED = 300
 const LASER_COOLDOWN = 0.5
 
+/*ENEMY PROPERTIES*/
 const ENEMIES_PER_ROW = 10
 const ENEMY_HORIZONTAL_PADDING = 80
 const ENEMY_VERTICAL_PADDING = 70
 const ENEMY_VERTICAL_SPACING = 80
+const ENEMY_COOLDOWN = 15.0;
 
 //stores everything happening in the game at any given time
 const GAME_STATE = {
@@ -27,12 +30,12 @@ const GAME_STATE = {
     playerY: 0,
     playerCooldown: 0,
     lasers: [],
-    enemies: []
+    enemies: [],
+    enemyLasers: [],
 }
 
 function rectsIntersect(r1, r2) {
-    /*if these conditions are met, the hitboxes are intersecting
-    not!*/
+    //if these conditions are NOT met, the hitboxes are intersecting
     return !(
         //if the left side of r2 is on the right of the right side of r1, hitbox cannot be intersecting
         r2.left > r1.right ||
@@ -54,6 +57,15 @@ function clamp(v, min, max) {
     if v is greater than max, return max, else return v
     ternary because fancy*/
     return v < min ? min : v > max ? max : v;
+}
+
+function rand(min, max) {
+    //sets the default values when not provided 
+    if (min === undefined) min = 0;
+    if (max === undefined) max = 1;
+    /*generate a value between 0 and 1, which is then multiplied 
+    by the range. minimum value is then added again*/
+    return min + Math.random() * (max - min);
 }
 
 function createPlayer($container) {
@@ -159,12 +171,12 @@ function updateLasers(deltaTime, $container) {
     GAME_STATE.lasers = GAME_STATE.lasers.filter(e => !e.isDead)
 }
 
-function updateLasers(dt, $container) {
+function updateLasers(deltaTime, $container) {
     const lasers = GAME_STATE.lasers
     for (let i = 0; i < lasers.length; i++) {
       const laser = lasers[i]
       //lasers go from bottom to top, therefore we subtract from their position
-      laser.y -= dt * LASER_MAX_SPEED
+      laser.y -= deltaTime * LASER_MAX_SPEED
       if (laser.y < 0) {
         //removing laser from the game state when it's about to leave bounds
         destroyLaser($container, laser)
@@ -199,29 +211,66 @@ function createEnemy($container, x, y) {
     $element.src = 'assets/images/enemy/Qual.png'
     $element.className = 'enemy'
     $container.appendChild($element)
-    const enemy = { x, y, $element }
+    const enemy = { 
+        x, 
+        y, 
+        cooldown: rand(0.5, ENEMY_COOLDOWN), 
+        $element 
+    }
     GAME_STATE.enemies.push(enemy)
     setPosition($element, x, y)
 }
 
-function updateEnemies() {
+function updateEnemies(deltaTime, $container) {
     //sine and cosine functions to create a wave-like movement
     const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
-  const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
+    const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
 
-  const enemies = GAME_STATE.enemies
-  for (let i = 0; i < enemies.length; i++) {
-    const enemy = enemies[i]
-    const x = enemy.x + dx
-    const y = enemy.y + dy
-    setPosition(enemy.$element, x, y);
-  }
-  GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
+    const enemies = GAME_STATE.enemies
+    for (let i = 0; i < enemies.length; i++) {
+        const enemy = enemies[i]
+        const x = enemy.x + dx
+        const y = enemy.y + dy
+        setPosition(enemy.$element, x, y);
+        enemy.cooldown -= deltaTime;
+        if (enemy.cooldown <= 0) {
+            createEnemyLaser($container, x, y);
+            enemy.cooldown = ENEMY_COOLDOWN;
+        }
+    }
+    GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
 }
 
 function destroyEnemy($container, enemy) {
     $container.removeChild(enemy.$element)
     enemy.isDead = true
+}
+
+/*enemy laser functions, among others in the roject, are almost 
+identical to the others, as they share many properties and behaviors. 
+this would be a good implementation of javascript classes in the future.*/
+function createEnemyLaser($container, x, y) {
+    const $element = document.createElement("img");
+    $element.src = "assets/images/laser/Zoltraak.png";
+    $element.className = "enemy-laser";
+    $container.appendChild($element);
+    const laser = { x, y, $element };
+    GAME_STATE.enemyLasers.push(laser);
+    setPosition($element, x, y);
+}
+  
+  function updateEnemyLasers(deltaTime, $container) {
+    const lasers = GAME_STATE.enemyLasers;
+    for (let i = 0; i < lasers.length; i++) {
+        const laser = lasers[i];
+        //enemy lasers move towards the bottom of the container
+        laser.y += deltaTime * LASER_MAX_SPEED;
+        if (laser.y > GAME_HEIGHT) {
+            destroyLaser($container, laser);
+        }
+        setPosition(laser.$element, laser.x, laser.y);
+    }
+    GAME_STATE.enemyLasers = GAME_STATE.enemyLasers.filter(e => !e.isDead);
 }
 
 //creates necessary elements for the game
@@ -232,11 +281,11 @@ function init () {
 
     const enemySpacing = (GAME_WIDTH - ENEMY_HORIZONTAL_PADDING * 2) / (ENEMIES_PER_ROW - 1)
     for (let j = 0; j < 3; j++) {
-      const y = ENEMY_VERTICAL_PADDING + j * ENEMY_VERTICAL_SPACING
-      for (let i = 0; i < ENEMIES_PER_ROW; i++) {
-        const x = i * enemySpacing + ENEMY_HORIZONTAL_PADDING
-        createEnemy($gameContainer, x, y)
-      }
+        const y = ENEMY_VERTICAL_PADDING + j * ENEMY_VERTICAL_SPACING
+        for (let i = 0; i < ENEMIES_PER_ROW; i++) {
+            const x = i * enemySpacing + ENEMY_HORIZONTAL_PADDING
+            createEnemy($gameContainer, x, y)
+        }
     }
 }
 
@@ -251,6 +300,7 @@ function update() {
     updatePlayer(deltaTime, $gameContainer)
     updateLasers(deltaTime, $gameContainer)
     updateEnemies(deltaTime, $gameContainer)
+    updateEnemyLasers(deltaTime, $gameContainer);
 
 
     //updating current time after the update function is called
@@ -259,7 +309,6 @@ function update() {
 }
 
 function onKeyDown(e) {
-    console.log(e)
     switch (e.key) {
         case 'ArrowRight':
             GAME_STATE.rightArrowPressed = true
@@ -280,7 +329,6 @@ function onKeyDown(e) {
 }
 
 function onKeyUp(e) {
-    // console.log(e.keyCode)
     switch (e.key) {
         case 'ArrowRight':
             GAME_STATE.rightArrowPressed = false
